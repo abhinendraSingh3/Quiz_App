15dcb8792aa0b45376d0526f9a4eb149d6d9aeed
Abhinendra Singh <abhinendrayadav3@gmail.com>
2022-10-13 01:43:00 +0530

    .

--- home/urls.py
+++ home/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from.import views
 urlpatterns = [
     path('',views.home,name='home'),
+    path('api/get-quiz', views.get_quiz,name='get_quiz')
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,26 @@
-from django.http import HttpResponse
+from cmath import e
+from django.http import HttpResponse, JsonResponse
+from .models import *
+import random
 # Create your views here.\
 def home(requests):
     return HttpResponse("hello bitches")
+
+def get_quiz(request):
+    try:
+        questionobj=Question.objects.all()
+        data=[]
+        for values in questionobj:
+            data.append({
+                "category":values.category.category_name,
+                 "question":values.question,
+                 "marks":values.marks
+            })
+
+        payload={'status':True,'data':data}
+
+        return JsonResponse(payload)
+
+    except Exception as e: 
+       print(e)
+    return HttpResponse("Something Went Wrong") 
